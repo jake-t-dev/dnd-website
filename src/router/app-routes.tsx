edbfe6fe41fb8b5484efb22d7543b6e681f9a62f
jake turner <krysis15@outlook.com>
2025-07-28 11:28:14 +0100

    feat: add char sidebar (#2)
    * add ci and page skeletons
    
    * disable eslint for line
    
    * add alais
    
    * move pnf to bottom

--- src/router/app-routes.tsx
+++ src/router/app-routes.tsx
@@ -1,15 +1,23 @@
+import Character from "@/pages/character/character";
+import Layout from "@/pages/character/layout/layout";
+import Spells from "@/pages/character/spells/spells";
+import Landing from "@/pages/landing";
 import PageNotFound from "@/pages/page-not-found";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* <Route path="/" element={<Landing />} /> */}
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/character" element={<Layout />}>
+          <Route index element={<Character />} />
+          <Route path="inventory" element={<Spells />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRoutes;
